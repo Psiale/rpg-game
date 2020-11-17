fd21147d4ccc9665b7cc85e04cf03d7c1b6e8ec0
Alexis <psialedev@gmail.com>
2020-11-17 14:18:52 -0600

    saving the player class

---
+++ src/classes/PlayerCharacter.js
@@ -0,0 +1,12 @@
+import 'phaser'
+import Unit from './Unit'
+
+export default class PlayerCharacter extends Unit {
+    constructor(scene, x, y, texture, frame, type, hp, damage) {
+        Unit.call(this, scene, x, y, texture, frame, type, hp, damage);
+        // flip the image so I don't have to edit it manually
+        this.flipX = true;
+        
+        this.setScale(2);
+    }
+}
