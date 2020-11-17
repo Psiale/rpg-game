141a80c65cbc98f34218bb4de8b226ad95423e96
Alexis <psialedev@gmail.com>
2020-11-17 12:08:02 -0600

    created new dir classes and added unit, enemy and player classes

---
+++ src/classes/Unit.js
@@ -0,0 +1,20 @@
+import 'phaser'
+
+
+export default class Unit {
+    constructor () {
+        super('Unit');
+    }
+
+    create() {
+        this.graphics = this.add.graphics();
+        this.graphics.lineStyle(1, 0xffffff);
+        this.graphics.fillStyle(0x031f4c, 1);        
+        this.graphics.strokeRect(2, 150, 90, 100);
+        this.graphics.fillRect(2, 150, 90, 100);
+        this.graphics.strokeRect(95, 150, 90, 100);
+        this.graphics.fillRect(95, 150, 90, 100);
+        this.graphics.strokeRect(188, 150, 130, 100);
+        this.graphics.fillRect(188, 150, 130, 100);
+        }
+}
