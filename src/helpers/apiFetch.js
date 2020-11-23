e41703d23351602967023ad7ec264d3410d62acd
Alexis <psialedev@gmail.com>
2020-11-23 16:54:40 -0600

    finished api testing

--- src/helpers/apiFetch.js
+++ src/helpers/apiFetch.js
@@ -2,27 +2,35 @@ import "regenerator-runtime";
 
 const fetch = require("node-fetch");
 
-// const initGame = async () => {
-// const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/'
-//   const title = JSON.stringify({
-//     name: 'Cuttle Little Town',
-//   });
-//    const data = {
-//     method: 'POST',
-//     headers: {
-//       Accept: 'application/json',
-//       'Content-Type': 'application/json',
-//     },
-//     body: title,
-//   };
+const initGame = async () => {
+  const baseUrl =
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
+  const title = JSON.stringify({
+    name: "an app for testing",
+  });
+  const data = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: title,
+  };
+  //
+  const response = await fetch(baseUrl, data);
+  const result = await response.json();
+  //
+  return result;
+};
+//
+const topScore = (arr) => {
+  try {
+    return arr.sort((a, b) => b.score - a.score);
 
-//   const response = await fetch(baseUrl, data);
-//   const result = await response.json();
-
-//   return result;
-// };
-
-const topScore = (arr) => arr.sort((a, b) => b.score - a.score);
+  } catch (error) {
+  throw new Error('Not enough elements to sort')    
+  }
+};
 
 const sendRequest = async (method, urlParam, userName = "", userScore = "") => {
   let data;
@@ -52,24 +60,40 @@ const sendRequest = async (method, urlParam, userName = "", userScore = "") => {
 
   method === "POST" ? (data = post) : get;
 
-  const baseUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/${urlParam}`;
+  try {
+    const baseUrl = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${apiKey}/${urlParam}`;
 
-  const response = await fetch(baseUrl, data);
-  const result = await response.json();
-  console.log(result);
-  return topScore(result.result);
-};
-
-const topFive = async (fn, self, fn2, param1, param2, param3, param4, param5) => {
-  if(await sendRequest('GET', 'scores').length == 0) {
-    fn2(param1, param2, param3, param4, param5, self)
-  }
-  const arr = await sendRequest( "GET", "scores");
-  for (let index = 0; index < 5; index++) {
-    if(index >= arr.length) break
-    const element = await arr[index];
-    fn(index, element, self);
+    const response = await fetch(baseUrl, data);
+    const result = await response.json();
+    return topScore(result.result);
+  } catch (error) {
+    throw new Error("there are not enough scores to sort");
   }
 };
 
-export { sendRequest, topFive };
+const topFive = async (
+  fn,
+  self,
+  fn2,
+  param1,
+  param2,
+  param3,
+  param4,
+  param5
+) => {
+  try {
+    if ((await sendRequest("GET", "scores").length) == 0) {
+      fn2(param1, param2, param3, param4, param5, self);
+    }
+    const arr = await sendRequest("GET", "scores");
+    for (let index = 0; index < 5; index++) {
+      if (index >= arr.length) break;
+      const element = await arr[index];
+      fn(index, element, self);
+    }
+  } catch (error) {
+    throw new Error("there are not enough elements to display a top 5");
+  }
+};
+
+export { sendRequest, topFive, initGame };
